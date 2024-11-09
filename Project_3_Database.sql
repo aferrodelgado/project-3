a0ed8b0426930b16070d9e984e8f0a90fd52cbf3
la220385 <la220385@ucf.edu>
2024-11-09 01:41:52 -0500

    Update Project_3_Database.sql

--- Project_3_Database.sql
+++ Project_3_Database.sql
@@ -32,3 +32,30 @@ CREATE TABLE usa_job_skills (
 	Job_Type VARCHAR(50),
 	Job_Skills VARCHAR(10000)
 );
+
+CREATE TABLE Industry_Demand (
+    State VARCHAR(50),
+    Total_Nonfarm DECIMAL(10, 2),
+    Mining_Logging DECIMAL(10, 2),
+    Mining_Logging_Construction DECIMAL(10, 2),
+    Construction DECIMAL(10, 2),
+    Manufacturing DECIMAL(10, 2),
+    Trade_Transportation_Utilities DECIMAL(10, 2),
+    Information DECIMAL(10, 2),
+    Financial_Activities DECIMAL(10, 2),
+    Professional_Business_Services DECIMAL(10, 2),
+    Education_Health_Services DECIMAL(10, 2),
+    Leisure_Hospitality DECIMAL(10, 2),
+    Other_Services DECIMAL(10, 2),
+    Government DECIMAL(10, 2)
+);
+
+CREATE TABLE Growth_Output_Per_Industry (
+    Industry_Description VARCHAR(200),
+    Sector VARCHAR(100),
+    NAICS_Code VARCHAR(50),
+    Output_2023 DECIMAL(10, 2),
+    Output_2033 DECIMAL(10, 2),
+    Employment_Change_Numeric DECIMAL(10, 2),
+    Compound_Annual_Rate_Change DECIMAL(5, 2)
+);
